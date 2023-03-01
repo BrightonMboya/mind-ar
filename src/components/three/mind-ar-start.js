462f17b066a01e897e411bbb1dd525fa15b9ac3f
BrightonMboya <b.mboya@alustudent.com>
2023-03-01 11:44:04 +0200

    configured the mindAR lib import error

---
+++ src/components/three/mind-ar-start.js
@@ -0,0 +1,33 @@
+//@ts-expect-error
+
+const THREE = window.MINDAR.IMAGE.THREE;
+
+document.addEventListener("DOMContentLoaded", () => {
+  const start = async () => {
+    const mindarThree = new window.MINDAR.IMAGE.MindARThree({
+      container: document.body,
+      imageTargetSrc: "../../public/targets.mind",
+    });
+
+    // getting the 3d stuff from the mindar obj
+    const { renderer, scene, camera } = mindarThree;
+    const geometry = new THREE.PlaneGeometry(1, 1);
+    const material = new THREE.MeshBasicMaterial({
+      color: 0x000ff,
+      transparent: true,
+      opacity: 0.5,
+    });
+
+    //creating a mindAr anchor point
+    const plane = new THREE.Mesh(geometry, material);
+    const anchor = mindarThree.addAnchor(0);
+    anchor.group.add(plane); // this is a Three Group element
+
+    await mindarThree.start();
+    renderer.setAnimationLoop(() => {
+      renderer.render(scene, camera);
+    });
+  };
+
+  start();
+});
---
+++ src/components/three/three.ts
@@ -0,0 +1,34 @@
+import * as THREE from "three";
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+
+const lineMaterial = new THREE.LineBasicMaterial({ color: 0x000ff });
+const points = [];
+points.push(new THREE.Vector3(- 10, 0, 0));
+points.push(new THREE.Vector3(0, 10, 0));
+points.push(new THREE.Vector3(10, 0, 0));
+const lineGeometry = new THREE.BufferGeometry().setFromPoints(points);
+
+const line = new THREE.Line(lineGeometry, lineMaterial);
+
+
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+scene.add(line)
+camera.position.z = 5;
+document.body.appendChild(renderer.domElement);
+
+function animate() {
+    requestAnimationFrame(animate);
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render(scene, camera);
+}
+
+animate();
